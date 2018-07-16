1b391970e5d80c7129971dedd1a0fe141dfa64ce
Talla <stalla@copperpoint.com>
2018-07-16 10:15:00 -0700

    r

--- src/First.java
+++ src/First.java
@@ -5,12 +5,5 @@ public class First {
 		System.out.println("Hello");
 		System.out.println("Hello2");
 		System.out.println("Hello3");
-<<<<<<< HEAD
-<<<<<<< HEAD
-		System.out.println("Hello90");
-=======
->>>>>>> parent of 69ce526... Third branch
-=======
->>>>>>> parent of 69ce526... Third branch
 	}
 }
